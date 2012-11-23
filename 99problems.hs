2783d7a2410fd521657dd4c3f28c9e48e6836cbb
Jordan Scales <scalesjordan@gmail.com>
2012-11-23 15:25:14 -0500

    problems 11 thru 20

--- 99problems.hs
+++ 99problems.hs
@@ -48,6 +48,7 @@ isPalindrone xs = xs == (myReverse xs)
 
 -- 7. (**) Flatten a nested list structure.
 data NestedList a = Elem a | List [NestedList a]
+
 flatten :: NestedList a -> [a]
 flatten (Elem x)      = [x]
 flatten (List [])     = []
@@ -83,3 +84,59 @@ pack' (x:xs) = (takeWhile (== x) (x:xs)) : (pack' $ dropWhile (== x) xs)
 --         are encoded as lists (N E) where N is the number of duplicates of the element E.
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode = map (\t -> (myLength t, head t)) . pack
+
+-- 11. (*) Modify the result of problem 10 in such a way that if an element has no duplicates it is simply 
+--         copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+data Grouping a = Multiple Int a | Single a deriving (Show)
+
+encodeModified :: (Eq a) => [a] -> [Grouping a]
+encodeModified = map transform . encode
+  where transform (n, c)
+          | n > 1     = Multiple n c
+          | otherwise = Single c
+
+-- 12. (**) Decode a run-length encoded list.
+decodeModified :: [Grouping a] -> [a]
+decodeModified = foldl (++) [] . map decode
+  where decode (Single c)     = [c]
+        decode (Multiple n c) = [c | _ <- [1..n]]
+
+-- 13. (**) Run-length encoding of a list (direct solution). 
+
+-- 14. (*) Duplicate the elements of a list.
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:dupli xs
+
+-- 15. (**) Replicate the elements of a list a given number of times.
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n = [x | _ <- [1..n]] ++ repli xs n
+
+-- 16. (**) Drop every N'th element from a list.
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery xs n = take (n-1) xs ++ dropEvery (drop n xs) n
+
+-- 17. (*) Split a list into two parts; the length of the first part is given.
+split :: [a] -> Int -> ([a], [a])
+split xs n = (take n xs, drop n xs)
+
+-- 18. (**) Extract a slice from a list.
+slice :: [a] -> Int -> Int -> [a]
+slice xs a b = take (b-a+1) $ drop (a-1) xs 
+
+-- 19. (**) Rotate a list N places to the left.
+rotate :: [a] -> Int -> [a]
+rotate xs n
+  | n < 0     = rotate xs (length xs + n)
+  | otherwise = (drop n xs) ++ (take n xs)
+
+-- 20. (*) Remove the K'th element from a list.
+removeAt :: [a] -> Int -> [a]
+removeAt [] _ = []
+removeAt (x:xs) 1 = xs
+removeAt (x:xs) n = x:removeAt xs (n-1)
+
+removeAt' :: [a] -> Int -> [a]
+removeAt' xs n = (take (n-1) xs) ++ (drop n xs)
